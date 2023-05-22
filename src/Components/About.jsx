b65d7e809bd75a67a731ed79103460885399f1e7
patrickmurayari <patrickmurayari03@gmail.com>
2023-05-21 21:39:45 -0300

    modificando componente Abaut 2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
     <div>
       <h1>About Page</h1>
       <h2>edmdmedkmede</h2>
+      <h2>edmdmedkmede</h2>
       <h2>ACA ESTUVO PATRICK</h2>
       <ul>
         <li>Maten a ese pavo</li>
