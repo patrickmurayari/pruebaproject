6e07dface64534e955e1005cadeba15c6d183d31
patrickmurayari <patrickmurayari03@gmail.com>
2023-05-21 21:23:37 -0300

    cambiando componente About

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,7 +5,10 @@ const About = () => {
   return (
     <div>
       <h1>About Page</h1>
-
+      <h2>ACA ESTUVO PATRICK</h2>
+      <ul>
+        <li>Maten a ese pavo</li>
+      </ul>
     </div>
   );
 };
